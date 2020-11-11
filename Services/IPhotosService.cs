f34d5cbeea3c0bbb401180c00ee1aec3543d5d87
Серёжа <gorbulevsv@yandex.ru>
2020-11-11 14:48:45 +0300

    2

---
+++ Services/IPhotosService.cs
@@ -0,0 +1,42 @@
+﻿using blazorHramPosts.Data;
+using blazorHramPosts.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blazorHramPosts.Services
+{
+    interface IPhotosService
+    {
+        Task<List<imageAlbum>> imageAlbums();
+        Task<imageAlbum> imageAlbum(int id);
+    }
+
+    public class PhotosService : IPhotosService
+    {
+        DbContextOptions<ApplicationDbContext> _options;
+
+        public PhotosService(ApplicationDbContext context, DbContextOptions<ApplicationDbContext> options)
+        {
+            _options = options;
+        }
+        
+        public async Task<List<imageAlbum>> imageAlbums()
+        {
+            using (var context=new ApplicationDbContext(_options))
+            {
+                return await context.imageAlbums.Include(i => i.images).ToListAsync();
+            }                
+        }
+        public async Task<imageAlbum> imageAlbum(int id)
+        {
+            using (var context = new ApplicationDbContext(_options))
+            {
+                return await context.imageAlbums.Include(i => i.images).FirstOrDefaultAsync(a => a.ID == id);
+            }            
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -67,6 +67,7 @@ namespace blazorHramPosts
             services.AddTransient<ICommentsService, CommentsService>();
             services.AddTransient<ILikesService, LikesService>();
             services.AddTransient<ISchedulesService, ScheduleService>();
+            services.AddTransient<IPhotosService, PhotosService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
